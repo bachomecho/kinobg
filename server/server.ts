376ad24a736406dba745dcd3d9698ad978c6547a
Ilia Mechkarov <64164772+bachomecho@users.noreply.github.com>
2024-04-23 18:07:42 +0300

    assets and server update

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { router } from "./lib/router.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json());
@@ -9,12 +11,18 @@ app.use(cors());
 
 app.use("/api", router);
 
-app.use("/images", express.static("dist/app/assets/static/images"));
-app.use(express.static("dist/app"));
+let filePath = "public";
+if (process.env.VITE_ENVIRONMENT === "PROD") {
+	filePath = "dist/app";
+}
+
+app.use("/images", express.static(`${filePath}/assets/static/images`));
+app.use("/icons", express.static(`${filePath}/assets/static/icons`));
+app.use(express.static(filePath));
 
 app.get("*", (_req, res) => {
-  const __dirname = process.cwd();
-  res.sendFile(path.join(__dirname, "./dist/app/index.html"));
+	const __dirname = process.cwd();
+	res.sendFile(path.join(__dirname, "./dist/app/index.html"));
 });
 
 const port = process.env.PORT || 3000;
